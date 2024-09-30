0849b1d6b3d2ad9a9750145f170fb7d8920dd16a
Xats89 <xats89@gmail.com>
2024-09-30 17:08:20 +0300

    feat: add couse content

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -5,25 +5,50 @@ export const myCV = {
     title: "О себе",
     text: "Инженерный склад ума, высшее техническое образование. Открыт для общения, порядочный, ответственный. В свободное время провожу время с друзьями и семьей. Продолжаю обучение Front-end разработке.",
   },
-  workExperience: [
-    {
-      title: "Опыт работы",
-      position: "Инженер-электроник",
-      job: "РУП “Белпочта”",
-      jobPeriod: "сентябрь 2016 - настоящее время",
-      responsibility: [
-        "Установка, настройка, техническое сопровождение ПО и оборудования",
-      ],
-    },
-  ],
-  education: [
-    {
-      title: "Образование",
-      university:
-        "Белорусский Государственный Университет Информатики и Радиоэлектроники",
-      qualification: "Инженер по радиоэлектронике",
-      studyPeriod: "2007 - 2012г"
-    },
-    
-  ],
+  workExperience: {
+    title: "Опыт работы",
+    content: [
+      {
+        position: "Инженер-электроник",
+        job: "РУП “Белпочта”",
+        jobPeriod: "сентябрь 2016 - настоящее время",
+        responsibility: [
+          "Установка, настройка, техническое сопровождение ПО и оборудования",
+        ],
+      },
+    ],
+  },
+  education: {
+    title: "Образование",
+    content: [
+      {
+        university:
+          "Белорусский Государственный Университет Информатики и Радиоэлектроники",
+        qualification: "Инженер по радиоэлектронике",
+        studyPeriod: "2007 - 2012г",
+      },
+    ],
+  },
+  courses: {
+    title: "Курсы",
+    content: [
+      {
+        school: "The Rolling Scopes School",
+        course: "JS/FE PRE-SCHOOL 2022Q4",
+        skills: [
+          "HTML, CSS & Git Basics",
+          "CSS Positioning, Flexbox & Grid",
+          "JavaScript Basics",
+          "DOM API & DOM Events",
+        ],
+        projects: [
+          {
+            label: "Plants",
+            link: "https://rolling-scopes-school.github.io/xats89-JSFEPRESCHOOL2022Q4/plants/",
+          },
+        ],
+        certificate: "https://app.rs.school/certificate/v0ohiv43"
+      },
+    ],
+  },
 };
